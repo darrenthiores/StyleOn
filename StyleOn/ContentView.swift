64fb0270e57755257ca8b91692bb27d486449a7b
darren thiorres <darrenthiores@gmail.com>
2024-05-21 19:34:43 +0700

    Initial Commit

---
+++ StyleOn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StyleOn
+//
+//  Created by Darren Thiores on 21/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StyleOn/StyleOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleOnApp.swift
+//  StyleOn
+//
+//  Created by Darren Thiores on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct StyleOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
